468a149a5e04ddf9fa956206526efd42a04ea1e4
andrei-tabachk <andrei.tabachk@gmail.com>
2023-06-06 19:59:24 -0400

    beginning of app

---
+++ app.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import pandas as pd
+
+from flask import Flask, jsonify, render_template
+
+engine = create_engine("")
