8fcb788c377dd2b7f44333b6d8d98350eaa860d6
andrei-tabachk <andrei.tabachk@gmail.com>
2023-06-06 21:01:02 -0400

    final push of structure

--- sqlite.py
+++ sqlite.py
@@ -8,6 +8,7 @@ from pathlib import Path
 # use pandas to create dataframe based on csv data
 df_airefare_decrease = pd.read_csv(r'C:\Users\andre\OneDrive\Documents\Columbia University - Class Work\Repositories\airlines_airfare\Consumer_Airfare_Report__Table_4_-_City-Pair_Markets_With_A_Substantial_Decrease_In_Average_Fare.csv')
 
+
 # create a sqlite database and a connection to it
 cnxn = sqlite3.connect('airfare_decrease.sqlite')
 
