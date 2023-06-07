917edd95f1bcbf830abb4fa0a3a3b85e6b2cf87d
andrei-tabachk <andrei.tabachk@gmail.com>
2023-06-06 21:40:05 -0400

    filtered table for 2017

--- sqlite.py
+++ sqlite.py
@@ -7,12 +7,12 @@ from pathlib import Path
 
 # use pandas to create dataframe based on csv data
 df_airefare_decrease = pd.read_csv(r'C:\Users\andre\OneDrive\Documents\Columbia University - Class Work\Repositories\airlines_airfare\Consumer_Airfare_Report__Table_4_-_City-Pair_Markets_With_A_Substantial_Decrease_In_Average_Fare.csv')
-
+filtered_airefare_decrease = df_airefare_decrease[df_airefare_decrease['Year'] == 2017]
 
 # create a sqlite database and a connection to it
 cnxn = sqlite3.connect('airfare_decrease.sqlite')
 
 # insert your dataframes into that database
-df_airefare_decrease.to_sql('airefare_decrease', cnxn, index=False)
+filtered_airefare_decrease.to_sql('airefare_decrease', cnxn, index=False)
 
 cnxn.close()
