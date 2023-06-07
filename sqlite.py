276b6fc431e01f59f80d7920edec18c7ee3ec274
andrei-tabachk <andrei.tabachk@gmail.com>
2023-06-06 20:16:54 -0400

    updated files

---
+++ sqlite.py
@@ -0,0 +1,15 @@
+# sqlite_demo.py
+
+import sqlite3
+import pandas as pd
+
+# use pandas to create dataframe based on csv data
+df_airefare_decrease = pd.read_csv(r'C:\Users\andre\OneDrive\Documents\Columbia University - Class Work\Repositories\airlines_airfare\Consumer_Airfare_Report__Table_4_-_City-Pair_Markets_With_A_Substantial_Decrease_In_Average_Fare.csv')
+
+# create a sqlite database and a connection to it
+cnxn = sqlite3.connect('airefare_decrease.db')
+
+# insert your dataframes into that database
+df_airefare_decrease.to_sql('airefare_decrease', cnxn, index=False)
+
+cnxn.close()
